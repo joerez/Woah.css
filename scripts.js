6c7ab73c5303a7c74e6effc5f53b60081885446d
Joe Rezendes <rezenbiz@gmail.com>
2018-03-02 14:02:52 -0800

    Animator working

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
+function testAnim(x) {
+  $('.box').removeClass().addClass('box' + ' woah ' + x, function(){
+    //$(this).removeClass();
+  });
+};
+
+
 $(document).ready(function()
 {
 
-let option = $('.picker').val();
-console.log(option);
 
-$('.subButton').on("click", function(e) {
-  $('.box').removeClass("woah comeInStyle");
-  $('.box').addClass("woah wowzors");
-})
 
-});
+  $('.subButton').click(function(e){
+        e.preventDefault();
+        var anim = $('.picker').val();
+        testAnim(anim);
+      });
+
+      $('.picker').change(function(){
+        var anim = $(this).val();
+        testAnim(anim);
+      });
+    });
+
+
+
+// let option = $('.picker').val();
+// console.log(option);
+//
+// $('.subButton').on("click", function(e) {
+//   $('.box').removeClass("woah comeInStyle");
+//   $('.box').addClass("woah wowzors");
+// })
+
+// });
